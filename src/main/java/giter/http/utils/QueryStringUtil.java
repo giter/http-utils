4b097b3f9ddb9374f67f7b745f13867bdec9fb82
giter <nubix@qq.com>
2012-12-11 22:35:24 +0800

    1. reformatter 2. delete obviously unused line

--- src/main/java/giter/http/utils/QueryStringUtil.java
+++ src/main/java/giter/http/utils/QueryStringUtil.java
@@ -9,101 +9,99 @@ import java.util.Map.Entry;
 
 public abstract class QueryStringUtil {
 
-	public static Map<String, String> parse(String queryString) {
-		return parse(queryString, "UTF-8");
-	}
+  public static Map<String, String> parse(String queryString) {
+    return parse(queryString, "UTF-8");
+  }
 
-	/**
-	 * Parse URL QueryString to Map<String,String>
-	 * 
-	 * @param queryString
-	 *          QueryString
-	 * @param encoding
-	 *          URL Encoding
-	 * @return If the queryString contains multiple parameters with a same name, first returned
-	 */
-	public static Map<String, String> parse(String queryString, String encoding) {
+  /**
+   * Parse URL QueryString to Map<String,String>
+   * 
+   * @param queryString
+   *          QueryString
+   * @param encoding
+   *          URL Encoding
+   * @return If the queryString contains multiple parameters with a same name, first returned
+   */
+  public static Map<String, String> parse(String queryString, String encoding) {
 
-		Map<String, String> params = new HashMap<>();
+    Map<String, String> params = new HashMap<>();
 
-		if (queryString == null) { return params; }
+    if (queryString == null) { return params; }
 
-		queryString = queryString.trim();
+    queryString = queryString.trim();
 
-		if (queryString.startsWith("?")) queryString = queryString.substring(1);
+    if (queryString.startsWith("?")) queryString = queryString.substring(1);
 
-		if (queryString.length() == 0) return params;
+    if (queryString.length() == 0) return params;
 
-		for (String s : queryString.split("&")) {
+    for (String s : queryString.split("&")) {
 
-			if (s.length() == 0) continue;
+      String k, v;
 
-			String k, v;
+      if (s.contains("=")) {
 
-			if (s.contains("=")) {
+        String[] pr = s.split("=", 2);
 
-				String[] pr = s.split("=", 2);
+        try {
+          k = URLDecoder.decode(pr[0].trim(), encoding);
+          v = URLDecoder.decode(pr[1].trim(), encoding);
+        } catch (UnsupportedEncodingException e) {
+          throw new RuntimeException(e);
+        }
 
-				try {
-					k = URLDecoder.decode(pr[0].trim(), encoding);
-					v = URLDecoder.decode(pr[1].trim(), encoding);
-				} catch (UnsupportedEncodingException e) {
-					throw new RuntimeException(e);
-				}
+      } else {
 
-			} else {
+        try {
+          k = URLDecoder.decode(s.trim(), encoding);
+        } catch (UnsupportedEncodingException e) {
+          throw new RuntimeException(e);
+        }
 
-				try {
-					k = URLDecoder.decode(s.trim(), encoding);
-				} catch (UnsupportedEncodingException e) {
-					throw new RuntimeException(e);
-				}
+        v = "";
+      }
 
-				v = "";
-			}
+      if (!params.containsKey(k)) {
+        params.put(k, v);
+      }
+    }
 
-			if (!params.containsKey(k)) {
-				params.put(k, v);
-			}
-		}
+    return params;
+  }
 
-		return params;
-	}
+  public static String query(Map<String, String> params) {
 
-	public static String query(Map<String, String> params) {
+    String query = "";
 
-		String query = "";
+    if (params != null && params.size() > 0) {
 
-		if (params != null && params.size() > 0) {
+      StringBuilder sb = new StringBuilder();
 
-			StringBuilder sb = new StringBuilder();
+      String pattern = "%s=%s";
 
-			String pattern = "%s=%s";
+      boolean first = true;
 
-			boolean first = true;
+      for (Entry<String, String> entry : params.entrySet()) {
 
-			for (Entry<String, String> entry : params.entrySet()) {
+        if (!first) {
+          sb.append("&");
+        } else {
+          first = false;
+        }
 
-				if (!first) {
-					sb.append("&");
-				} else {
-					first = false;
-				}
+        try {
+          sb.append(String.format(pattern, URLEncoder.encode(entry.getKey(), "UTF-8"),
+              URLEncoder.encode(entry.getValue(), "UTF-8")));
+        } catch (UnsupportedEncodingException e) {
+          throw new RuntimeException(e);
+        }
+      }
 
-				try {
-					sb.append(String.format(pattern, URLEncoder.encode(entry.getKey(), "UTF-8"),
-							URLEncoder.encode(entry.getValue(), "UTF-8")));
-				} catch (UnsupportedEncodingException e) {
-					throw new RuntimeException(e);
-				}
-			}
+      if (sb.length() > 0) {
+        query = sb.toString();
+      }
 
-			if (sb.length() > 0) {
-				query = sb.toString();
-			}
+    }
 
-		}
-
-		return query;
-	}
+    return query;
+  }
 }
